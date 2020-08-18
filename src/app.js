8f60692f24cb224b2701679578a19e99d13d132a
Görkem <gumutzkn@gmail.com>
2020-08-18 16:39:41 +0300

    new class

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 import Paddle from "./paddle.js";
+import InputHandler from "./input.js";
+import Ball from "./ball.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-const GAME_WIDTH = 1000;
-const GAME_HEIGHT = 600;
-
-ctx.clearRect(0, 0, 1000, 600);
+const GAME_WIDTH = 1200;
+const GAME_HEIGHT = 800;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-paddle.draw(ctx);
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
+
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+const gameLoop = (timestamp) => {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  ball.update(deltaTime);
+  ball.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+};
+
+requestAnimationFrame(gameLoop);
