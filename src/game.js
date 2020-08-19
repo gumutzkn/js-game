c4ea63ea20c0a2a6f19f6387167543e14ffc8586
Görkem <gumutzkn@gmail.com>
2020-08-19 17:57:15 +0300

    pause the game

--- src/game.js
+++ src/game.js
@@ -4,6 +4,13 @@ import Ball from "./ball.js";
 import Brick from "./brick.js";
 import { buildLevel, level1 } from "./levels.js";
 
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3,
+};
+
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
@@ -11,6 +18,7 @@ export default class Game {
   }
 
   start() {
+    this.gamestate = GAMESTATE.RUNNING;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
 
@@ -18,10 +26,12 @@ export default class Game {
 
     this.gameObjects = [this.ball, this.paddle, ...bricks];
 
-    new InputHandler(this.paddle);
+    new InputHandler(this.paddle, this);
   }
 
   update(deltaTime) {
+    if (this.gamestate == GAMESTATE.PAUSED) return;
+
     this.gameObjects.forEach((object) => object.update(deltaTime));
 
     this.gameObjects = this.gameObjects.filter((object) => !object.markedForDeletion);
@@ -29,5 +39,13 @@ export default class Game {
 
   draw(ctx) {
     this.gameObjects.forEach((object) => object.draw(ctx));
+  }
+
+  togglePause() {
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
   }
 }
